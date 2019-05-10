d1ac87daa12118343fb02065bfde4a1d3cf83666
Преподаватель <teacher@kr.step.ua>
2019-05-10 19:54:32 +0300

    commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OpenFolder = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox_Filter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_OpenFolder
+            // 
+            this.btn_OpenFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_OpenFolder.Location = new System.Drawing.Point(144, 63);
+            this.btn_OpenFolder.Name = "btn_OpenFolder";
+            this.btn_OpenFolder.Size = new System.Drawing.Size(233, 53);
+            this.btn_OpenFolder.TabIndex = 0;
+            this.btn_OpenFolder.Text = "OpenFolder";
+            this.btn_OpenFolder.UseVisualStyleBackColor = true;
+            this.btn_OpenFolder.Click += new System.EventHandler(this.btn_OpenFolder_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 122);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(514, 277);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(174, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filter";
+            // 
+            // txtBox_Filter
+            // 
+            this.txtBox_Filter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtBox_Filter.Location = new System.Drawing.Point(240, 18);
+            this.txtBox_Filter.Name = "txtBox_Filter";
+            this.txtBox_Filter.Size = new System.Drawing.Size(100, 31);
+            this.txtBox_Filter.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 448);
+            this.Controls.Add(this.txtBox_Filter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_OpenFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OpenFolder;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox_Filter;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        string fileName = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OpenFolder_Click(object sender, EventArgs e)
+        {
+            
+            
+            FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
+            if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            {
+                fileName = directchoosedlg.SelectedPath;
+                
+            }
+            string[] filesNames = Directory.GetFiles(fileName);
+            List<string> filesNamesWithFilter = new List<string>();
+            foreach (string item in filesNames)
+            {
+                if(new FileInfo(item).Extension == txtBox_Filter.Text)
+                {
+                    filesNamesWithFilter.Add(item);
+                }
+            }
+
+            foreach (string item in filesNamesWithFilter)
+            {
+                listBox1.Items.Add(Path.GetFileName(item));
+            }
+        }
+    }
+}
